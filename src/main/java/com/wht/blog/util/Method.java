be1b10f99ab3ad8440d374bd76b243243fd08343
wanghaitao <15124505701@163.com>
2019-11-05 05:07:31 +0800

    feat: del html special String

--- src/main/java/com/wht/blog/util/Method.java
+++ src/main/java/com/wht/blog/util/Method.java
@@ -126,18 +126,21 @@ public class Method {
     public static String delHTMLTag(String htmlStr) {
         String regEx_script = "<script[^>]*?>[\\s\\S]*?</script>"; //定义script的正则表达式
         String regEx_style = "<style[^>]*?>[\\s\\S]*?</style>"; //定义style的正则表达式
+        String regEx_string = "&nbsp;"; //定义特殊字符串
         String regEx_html = "<[^>]+>"; //定义HTML标签的正则表达式
-        Pattern p_script = Pattern.compile(regEx_script, Pattern.CASE_INSENSITIVE);
-        Matcher m_script = p_script.matcher(htmlStr);
-        htmlStr = m_script.replaceAll(""); //过滤script标签
+        htmlStr = getString(htmlStr, regEx_style, regEx_script);
+        htmlStr = getString(htmlStr, regEx_string, regEx_html);
+        return htmlStr.trim(); //返回文本字符串
+    }
 
-        Pattern p_style = Pattern.compile(regEx_style, Pattern.CASE_INSENSITIVE);
-        Matcher m_style=p_style.matcher(htmlStr);
-        htmlStr = m_style.replaceAll(""); //过滤style标签
+    private static String getString(String htmlStr, String regEx_string, String regEx_html) {
         Pattern p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE);
         Matcher m_html = p_html.matcher(htmlStr);
         htmlStr = m_html.replaceAll(""); //过滤html标签
-        return htmlStr.trim(); //返回文本字符串
+        Pattern p_string = Pattern.compile(regEx_string, Pattern.CASE_INSENSITIVE);
+        Matcher m_string = p_string.matcher(htmlStr);
+        htmlStr = m_string.replaceAll(""); //过滤特殊字符
+        return htmlStr;
     }
 
     /**
